5173933be9d4ca29721c068453c37e8586629ecb
Tom Robertshaw <me@tomrobertshaw.net>
2016-05-20 10:23:34 +0100

    Fixes #24: Pass CSV paths into Carrier constructor.

--- src/Carrier.php
+++ src/Carrier.php
@@ -3,9 +3,6 @@
 class Carrier implements CarrierInterface
 {
 
-    // Helper document root
-    public $documentRoot;
-
     /**
      * CSV file location for CountryCodes
      * @var string
@@ -56,20 +53,33 @@ class Carrier implements CarrierInterface
     protected $data;
 
     /**
-     * CalculateMethod constructor.
-     * @param null|Data $data
+     * Carrier constructor.
+     *
+     * @param string|null $csvCountryCode
+     * @param string|null $csvZoneToDeliveryMethod
+     * @param string|null $csvDeliveryMethodMeta
+     * @param string|null $csvDeliveryToPrice
+     * @param string|null $csvCleanNameToMethod
+     * @param string|null $csvCleanNameMethodGroup
      */
-    public function __construct($data = null)
+    public function __construct($csvCountryCode = null, $csvZoneToDeliveryMethod = null, $csvDeliveryMethodMeta = null,
+                                $csvDeliveryToPrice = null, $csvCleanNameToMethod = null,
+                                $csvCleanNameMethodGroup = null)
     {
-        $this->getDocumentRoot();
+        $dir = dirname(realpath(__FILE__)) . '/';
 
         // Set the default csv values
-        $this->_csvCountryCode = $this->documentRoot . '../data/1_countryToZone.csv';
-        $this->_csvZoneToDeliveryMethod = $this->documentRoot . '../data/2_zoneToDeliveryMethod.csv';
-        $this->_csvDeliveryMethodMeta = $this->documentRoot . '../data/3_deliveryMethodMeta.csv';
-        $this->_csvDeliveryToPrice = $this->documentRoot . '../data/4_deliveryToPrice.csv';
-        $this->_csvCleanNameToMethod = $this->documentRoot . '../data/5_cleanNameToMethod.csv';
-        $this->_csvCleanNameMethodGroup = $this->documentRoot . '../data/6_cleanNameMethodGroup.csv';
+        $this->_csvCountryCode = isset($csvCountryCode) ? $csvCountryCode : $dir . '../data/1_countryToZone.csv';
+        $this->_csvZoneToDeliveryMethod = isset($csvZoneToDeliveryMethod) ? $csvZoneToDeliveryMethod :
+            $dir . '../data/2_zoneToDeliveryMethod.csv';
+        $this->_csvDeliveryMethodMeta = isset($csvDeliveryMethodMeta) ? $csvDeliveryMethodMeta :
+            $dir . '../data/3_deliveryMethodMeta.csv';
+        $this->_csvDeliveryToPrice = isset($csvDeliveryToPrice) ? $csvDeliveryToPrice :
+            $dir . '../data/4_deliveryToPrice.csv';
+        $this->_csvCleanNameToMethod = isset($csvCleanNameToMethod) ? $csvCleanNameToMethod :
+            $dir . '../data/5_cleanNameToMethod.csv';
+        $this->_csvCleanNameMethodGroup = isset($csvCleanNameMethodGroup) ? $csvCleanNameMethodGroup :
+            $dir . '../data/6_cleanNameMethodGroup.csv';
 
         $this->data = isset($data) ? $data : new Data(
             $this->_csvCountryCode,
@@ -133,14 +143,6 @@ class Carrier implements CarrierInterface
         }
 
         return $methods;
-    }
-
-    /**
-     * Helper function to get the document root for csv files
-     */
-    public function getDocumentRoot()
-    {
-        $this->documentRoot = dirname(realpath(__FILE__)) . '/';
     }
 
     /**
